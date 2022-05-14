59722f8b7d12fd9c4e916f3a1c778c14058f7674
blincotaylor <blincotaylor@gmail.com>
2022-05-14 20:08:26 +0100

    changed html structure slightly and got js working fully

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 "use strict";
 
-const accordionLabels = document.querySelectorAll(".accordion__label");
-const accordionContent = document.querySelectorAll(".accordion__content");
+const accordionLabel = document.querySelectorAll(".accordion__label");
 
-for (let i = 0; i < accordionLabels.length; i++) {
-  accordionLabels[i].addEventListener("click", labelExpand);
-}
-
-function labelExpand() {}
+// add listener for click on each accordion__label
+accordionLabel.forEach((question) =>
+  question.addEventListener("click", () => {
+    // if clicked on accordion__label's parent has active class then toggle it off
+    if (question.parentNode.classList.contains("active")) {
+      question.parentNode.classList.toggle("active");
+    }
+    // otherwise add it
+    else {
+      question.parentNode.classList.add("active");
+    }
+  })
+);
