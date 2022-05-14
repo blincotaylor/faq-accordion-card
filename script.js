e562f48151e58d81c8cfd429ac72dd9be6e38e9c
blincotaylor <blincotaylor@gmail.com>
2022-05-14 22:04:26 +0100

    refactored js (made toggle rather than needless if statement)

--- script.js
+++ script.js
@@ -5,13 +5,6 @@ const accordionLabel = document.querySelectorAll(".accordion__label");
 // add listener for click on each accordion__label
 accordionLabel.forEach((question) =>
   question.addEventListener("click", () => {
-    // if clicked on accordion__label's parent has active class then toggle it off
-    if (question.parentNode.classList.contains("active")) {
-      question.parentNode.classList.toggle("active");
-    }
-    // otherwise add it
-    else {
-      question.parentNode.classList.add("active");
-    }
+    question.parentNode.classList.toggle("active");
   })
 );
